b678de0b93ff3d677177ab80fd161c0b0f2b52d1
yumwaikit <yumwaikit@gmail.com>
2020-04-16 16:55:43 +0800

    maniputator test

---
+++ scripts/manipulator_node.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import math
+import rospy
+from std_msgs.msg import Float64MultiArray
+
+def manipulator_node():
+    joint_publisher = rospy.Publisher('joint_trajectory_point', Float64MultiArray, queue_size = 10)
+    gripper_publisher = rospy.Publisher('gripper_position', Float64MultiArray, queue_size = 10)
+    rospy.init_node('manipulator_node')
+    r = rospy.Rate(1)
+
+    while not rospy.is_shutdown():
+        joints = Float64MultiArray()
+        joints.data = [0.0, 0.0, 0.0, -math.pi / 2, 0.0]
+        gripper = Float64MultiArray()
+        gripper.data = [0.0]
+        joint_publisher.publish(joints)
+        gripper_publisher.publish(gripper)
+    	r.sleep()
+
+if __name__=="__main__":
+    try:
+        manipulator_node()
+    except rospy.ROSInterruptException:
+        pass
